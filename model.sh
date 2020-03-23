2da4dbd99257a5ba0feef1e9ab44a356214cca30
Soomin Kimu <soominkimu@gmail.com>
2020-03-24 08:56:12 +0900

    model.sh

---
+++ model.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+curl -s https://support-sp.apple.com/sp/product?cc=$(
+system_profiler SPHardwareDataType \
+    | awk '/Serial/ {print $4}' \
+    | cut -c 9-
+) | sed 's|.*<configCode>\(.*\)</configCode>.*|\1|'
